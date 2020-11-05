c873c24fb6e76d3ae25733ffeed1956b04e6a80c
Bence Boka <boka.bence@gmail.com>
2020-11-05 02:08:16 +0000

    first

---
+++ mymemory.c
@@ -0,0 +1,142 @@
+/* mymemory.c
+ * 
+ * provides interface to memory management
+ * 
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include "mymemory.h"
+
+
+// our memory
+Byte        mymemory [MAXMEM] ;
+Segment_t * segmenttable = NULL;
+
+
+void initialize ()
+{
+   printf ( "initialize> start\n");
+   // set memory to 0
+    int mymemory [MAXMEM];
+   // create segment table
+   // contains one segment description that declares
+   //  the whole memory as one free segment
+   //
+   // create a single segment descriptor
+    segmenttable = (Segment_t *) (mymemory);
+   // initialise the segment
+    segmenttable->allocated = FALSE;
+    segmenttable->size = MAXMEM;
+    segmenttable->start = &mymemory[0];
+
+   printf ( "initialize> end\n");
+}
+
+//void myfree ( void * ptr )
+
+void * mymalloc ( size_t size )
+{
+   printf ( "mymalloc> start\n");
+   Segment_t* SegmentD = segmenttable->next;
+   Segment_t* Finder = findFree(segmenttable, size);
+   // implement the mymalloc functionality
+   if (Finder != NULL && Finder->start == NULL){
+
+   }
+   return ;
+}
+
+void myfree ( void * ptr )
+{
+   printf ( "myfree> start\n");
+
+}
+
+void mydefrag ( void ** ptrlist)
+{
+   printf ( "mydefrag> start\n");
+
+}
+
+
+// helper functions for management segmentation table
+Segment_t * findFree ( Segment_t * list, size_t size )
+{
+   printf ( "findFree> start\n");
+   int i = 0;
+   for (i; i<MAXSEGMENTS; ++i) //searching free segment
+   {    
+      if (list->allocated==FALSE && list->size >= size)
+      {
+         return list; //found the free segment
+      }
+      else
+      {
+         list = list->next; //going for the next segment
+         if (list==NULL){
+            return NULL;//the next segment does not exist
+         }
+         else{
+            continue; //im not sure if i need this continue
+         }             //as it should continue without anyways
+      }   
+   }
+   return NULL;//over max segment, memeory is full or 
+               //not enough free space in any segments
+}
+
+void insertAfter ( Segment_t * oldSegment, Segment_t * newSegment )
+{
+   oldSegment->next == newSegment;
+}
+
+Segment_t * findSegment ( Segment_t * list, void * ptr )
+{
+}
+
+int isPrintable ( int c )
+{
+   if ( c >= 0x20 && c <= 0x7e ) return c ;
+
+   return 0 ;
+}
+
+void printmemory ()
+{
+   int i;
+   int j;
+   int c;
+    for (i=0 ,j=10; i<MAXMEM; j+=10)
+    {
+        printf("\n[%4d]",j-10);
+        for (i,j; i<j && i<MAXMEM; ++i)
+        {
+            c = isPrintable(mymemory[i]);
+            printf(" %02x",mymemory[i]);
+            printf("|%s ",(c == 0 ? "." : mymemory[i]));
+        }
+    }
+}
+
+void printsegmenttable()
+{
+   int i = 0;
+   while (i<MAXSEGMENTS){ //nem NULL csak az elsö
+      printf("\n");
+      printf("\nSegment %d\n", i);
+      printsegmentdescriptor(segmenttable[i]);
+      ++i;
+      if (segmenttable->next == NULL){
+         break;
+      }
+   }
+}
+
+void printsegmentdescriptor ( Segment_t * descriptor )
+{
+      printf ( "\tallocated = %s\n" , (descriptor->allocated == FALSE ? "FALSE" : "TRUE" ) ) ;
+      printf ( "\tstart     = %p\n" , descriptor->start ) ;
+      printf ( "\tsize      = %lu\n", descriptor->size  ) ;
+}
---
+++ shell.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "mymemory.h"
+
+int main()
+{
+    printf ( "shell> start\n");
+    initialize() ;
+    char * ptr1 = (char *) mymalloc ( 10 ) ;
+    strcpy (ptr1, "this test");
+    printf( "shell> content of allocated memory: %s\n", ptr1 ) ;
+    char * ptr2 = (char *) mymalloc ( 10 ) ;
+    strcpy (ptr2, "this test");
+    printf( "shell> content of allocated memory: %s\n", ptr2 ) ;
+    printmemory() ;
+    printsegmenttable() ;
+    printf ( "shell> end\n");
+    return 0;
+}
